e857216fe64fc4e83a9ac61ffbf364a3a28b1206
tianaFomicheva <tianirika@gmail.com>
2021-03-21 11:34:32 +0500

    форматирование  и удаление лишнего

--- src/components/CategoryColumn.jsx
+++ src/components/CategoryColumn.jsx
@@ -1,76 +1,69 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import {TodoItem,  AddForm} from './index'
-import { useDrop} from 'react-dnd'
+import { TodoItem, AddForm } from './index'
+import { useDrop } from 'react-dnd'
 import constants from '../constants.js'
-import {editTaskCategory} from '../redux/actions/tasks'
-import {  useDispatch} from 'react-redux'
+import { editTaskCategory } from '../redux/actions/tasks'
+import { useDispatch } from 'react-redux'
 
 
 
-
-
-    
-
-function CategoryColumn({title, categoryId, todos,count}) {
+function CategoryColumn({ title, categoryId, todos, count }) {
 
   const dispatch = useDispatch()
 
-let position = [categoryId,0]
-const [x,y] = position
-
-const [, drop] = useDrop(
-  () => ({
-    accept: constants.CARD,
-    collect:(monitor) => ({
-      isOver: monitor.isOver(),
+  const [, drop] = useDrop(
+    () => ({
+      accept: constants.CARD,
+      collect: (monitor) => ({
+        isOver: monitor.isOver(),
       }),
-    drop: (monitor) => {
-      const obj = {
-        id: monitor.itemId,
-        text: monitor.itemText,
-        categoryId
-      }       
-      dispatch(editTaskCategory(obj))
-      
-    }
-  }),
-  [x, y]
-)
-    return (
-        <div ref={drop}>
-         
-            <div className="category-title"> {title} - {count}</div>
-            <ul className="task-item">
-              {todos && todos.map((todo)=> 
-                <div id={'item'+todo.id}  key={todo.id}><TodoItem  id={todo.id} key={todo.id}  text={todo.text}/></div>
-                 
-                )}
+      drop: (monitor) => {
+        const obj = {
+          id: monitor.itemId,
+          text: monitor.itemText,
+          categoryId
+        }
+        dispatch(editTaskCategory(obj))
 
-            </ul>
+      }
+    }),
+    []
+  )
+  return (
+    <div ref={drop}>
 
-            <AddForm categoryId={categoryId}/>
-           
-        </div>
-    )
+      <div className="category-title"> {title} - {count}</div>
+      <ul className="task-item">
+        {todos && todos.map((todo) =>
+          <div id={'item' + todo.id} key={todo.id}><TodoItem id={todo.id} key={todo.id} text={todo.text} /></div>
+
+        )}
+
+      </ul>
+
+      <AddForm categoryId={categoryId} />
+
+    </div>
+  )
 }
 CategoryColumn.propTypes = {
-   title: PropTypes.string,
-   categoryId: PropTypes.number,
-   todos: PropTypes.array,
-   onAddTask: PropTypes.func,
-   count: PropTypes.number,
-   updateTaskColumn: PropTypes.func,
+  title: PropTypes.string,
+  categoryId: PropTypes.number,
+  todos: PropTypes.array,
+  onAddTask: PropTypes.func,
+  count: PropTypes.number,
+  updateTaskColumn: PropTypes.func,
 
 
-    
-  }
-  
-  CategoryColumn.defaultProps = {
-    title: '---',
-    categoryId: null,
-    
-  }
-  
-  export default CategoryColumn
+
+}
+
+CategoryColumn.defaultProps = {
+  title: '---',
+  categoryId: null,
+
+}
+
+export default CategoryColumn
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,27 +3,27 @@ import CategoryColumn from './CategoryColumn'
 import Filter from './Filter'
 import { useSelector } from 'react-redux'
 import { filterTasks } from '../redux/actions/tasks'
-import { useDispatch} from 'react-redux'
+import { useDispatch } from 'react-redux'
 import PropTypes from 'prop-types'
 
 function TodoList() {
     const dispatch = useDispatch()
     const categoryNames = ['Бэклог', 'Надо сделать', 'В работе', 'Проверка', 'Сделано']
 
-          const todos = useSelector(tasks => (tasks.filtered)  ? tasks.filtered  : ((tasks.items && tasks.items.length> 0) ?  tasks.items:[]))
-    const handleFiltered = (filter)=>{
+    const todos = useSelector(tasks => (tasks.filtered) ? tasks.filtered : ((tasks.items && tasks.items.length > 0) ? tasks.items : []))
+    const handleFiltered = (filter) => {
         dispatch(filterTasks(filter))
-    }   
-    const cancelFiltered = ()=>{
+    }
+    const cancelFiltered = () => {
         dispatch(filterTasks(''))
-    }   
+    }
 
     return (
         <div>
             <Filter onFiltered={handleFiltered} />
             <div className="content">
 
-                {categoryNames.map((categoryName, index) => <CategoryColumn key={index} categoryId={index} title={categoryName}  todos={(todos.filter(t => t.categoryId == index))} count={(todos.filter(t => t.categoryId == index)).length} onCancelFiltered={cancelFiltered}/>)}
+                {categoryNames.map((categoryName, index) => <CategoryColumn key={index} categoryId={index} title={categoryName} todos={(todos.filter(t => t.categoryId == index))} count={(todos.filter(t => t.categoryId == index)).length} onCancelFiltered={cancelFiltered} />)}
             </div>
         </div>
     )
